61fdb2e1692e2e9661ac1c3eac3e85f6bd8944e0
yanyi0 <785144130@qq.com>
2021-09-06 22:42:31 +0800

    lamda表达式捕获外界变量

---
+++ 041-Lamda捕获外界变量/041-Lamda捕获外界变量/main.cpp
@@ -0,0 +1,8 @@
+//
+//  main.cpp
+//  041-Lamda捕获外界变量
+//
+//  Created by Cloud on 2021/9/6.
+//
+
+#include "main.hpp"
---
+++ 041-Lamda捕获外界变量/041-Lamda捕获外界变量/main.hpp
@@ -0,0 +1,13 @@
+//
+//  main.hpp
+//  041-Lamda捕获外界变量
+//
+//  Created by Cloud on 2021/9/6.
+//
+
+#ifndef main_hpp
+#define main_hpp
+
+#include <stdio.h>
+
+#endif /* main_hpp */
