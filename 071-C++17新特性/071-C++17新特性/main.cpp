42e589fc4d9848d37b57eb6bb6792899df86e454
yanyi0 <785144130@qq.com>
2021-10-06 19:19:57 +0800

    C++17新特性

---
+++ 071-C++17新特性/071-C++17新特性/main.cpp
@@ -0,0 +1,30 @@
+//
+//  main.cpp
+//  071-C++17新特性
+//
+//  Created by Cloud on 2021/10/6.
+//
+
+#include "main.hpp"
+#include <iostream>
+using namespace std;
+int main(){
+    if (int a = 10;a>10) {
+        
+    }else{
+        
+    }
+    switch (int b = 10;b) {
+        case 1:
+            
+            break;
+        case 2:
+            break;
+        case 10:
+            break;
+        default:
+            break;
+    }
+    getchar();
+    return 0;
+}
---
+++ 071-C++17新特性/071-C++17新特性/main.hpp
@@ -0,0 +1,13 @@
+//
+//  main.hpp
+//  071-C++17新特性
+//
+//  Created by Cloud on 2021/10/6.
+//
+
+#ifndef main_hpp
+#define main_hpp
+
+#include <stdio.h>
+
+#endif /* main_hpp */
